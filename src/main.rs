079a3ce192f31fcbc7efc7799fdb9665b893427b
jens1o <jenshausdorf@gmail.com>
2018-09-08 11:07:51 +0200

    added chrome browser, added a loop in the main function, implemented traits, doc

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,70 @@
+//! # Rust UA Matcher
+//! Simple Rust application which extracts the Browser(the name an the used version)
+//! from the user-agent string.
+
 extern crate regex;
 #[macro_use]
 extern crate lazy_static;
 
-use regex::Regex;
+use regex::{Regex, RegexBuilder};
 use std::collections::HashMap;
-use std::io;
-use std::io::Write;
+use std::fmt::{self, Display};
+use std::io::{self, Write};
 
+/// Represents a Browser
 #[derive(Debug)]
-struct Browser<'t> {
+pub struct Browser<'t> {
     display_name: &'static str,
     version: &'t str,
 }
 
+impl<'t> PartialEq for Browser<'t> {
+    fn eq(&self, other: &Browser) -> bool {
+        self.display_name == other.display_name && self.version == other.version
+    }
+}
+
+impl<'t> Display for Browser<'t> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "Browser: {} Version: {}",
+            self.display_name, self.version
+        )
+    }
+}
+
 lazy_static! {
-    static ref FIREFOX_REGEX: Regex = { Regex::new(r"firefox/([\d\.]+)").unwrap() };
-    static ref EDGE_REGEX: Regex = { Regex::new(r"edge/(\d{2}\.\d+)").unwrap() };
+    static ref FIREFOX_REGEX: Regex = {
+        RegexBuilder::new(r"firefox/([\d\.]+)")
+            .case_insensitive(true)
+            .build()
+            .unwrap()
+    };
+    static ref EDGE_REGEX: Regex = {
+        RegexBuilder::new(r"edge/(\d{2}\.\d+)")
+            .case_insensitive(true)
+            .build()
+            .unwrap()
+    };
+    static ref CHROME_REGEX: Regex = {
+        RegexBuilder::new(r"(?:chromium|chrome)/([\d\.]+)")
+            .case_insensitive(true)
+            .build()
+            .unwrap()
+    };
     static ref BROWSER_LIST: HashMap<&'static str, &'static Regex> = {
         let mut m = HashMap::new();
 
         m.insert("Firefox", &*FIREFOX_REGEX);
         m.insert("Edge", &*EDGE_REGEX);
+        m.insert("Chrome", &*CHROME_REGEX);
 
         m
     };
 }
 
 fn main() {
-    let mut user_agent = String::new();
     let title = "User-Agent matcher";
 
     println!("{}", title);
@@ -35,23 +72,30 @@ fn main() {
     println!("");
     println!("Enter an User-Agent string to get the version string.");
 
-    print!("> ");
-    io::stdout().flush().unwrap();
+    loop {
+        print!("> ");
+        io::stdout().flush().unwrap();
 
-    io::stdin().read_line(&mut user_agent).unwrap();
+        let mut user_agent = String::new();
 
-    let user_agent: &str = user_agent.trim();
+        io::stdin().read_line(&mut user_agent).unwrap();
 
-    println!("User-Agent: {}", user_agent);
+        let user_agent: &str = user_agent.trim();
 
-    if let Some(ref browser) = get_browser(user_agent) {
-        println!("Found a result: {:?}", browser);
-    } else {
-        println!("No result :/");
+        println!("User-Agent: {}", user_agent);
+
+        if let Some(ref browser) = get_browser(user_agent) {
+            println!("{}", browser);
+        } else {
+            println!("No result :/");
+        }
     }
 }
 
-fn get_browser(user_agent: &str) -> Option<Browser> {
+/// Iterates on every known browser (precompiled-)regex
+/// and checks whether it matches. If so, it returns a [Browser](struct.browser.html)
+/// object.
+pub fn get_browser(user_agent: &str) -> Option<Browser> {
     for (display_name, regex) in BROWSER_LIST.iter() {
         if let Some(ref matches) = regex.captures(user_agent) {
             return Some(Browser {
@@ -62,4 +106,27 @@ fn get_browser(user_agent: &str) -> Option<Browser> {
     }
 
     None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{get_browser, Browser};
+
+    #[test]
+    fn test_detects_browser() {
+        let user_agent_string =
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:64.0) Gecko/20100101 Firefox/64.0";
+
+        let result = get_browser(user_agent_string);
+
+        assert!(result.is_some());
+
+        assert_eq!(
+            result.unwrap(),
+            Browser {
+                display_name: "Firefox",
+                version: "64.0"
+            }
+        )
+    }
 }
